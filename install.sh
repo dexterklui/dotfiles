1bccdfde7bcd761148942494e7c723e47f850adc
Dexter Lui <dexq@DQ-x1c>
2020-05-16 15:32:16 +0900

    Fixed install location for tridactylrc.

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ cd $BASEDIR || exit
 
 ln -s ${PWD}/bashrc ~/.bashrc
 ln -s ${PWD}/vimrc ~/.vimrc
-ln -s ${PWD}/tridactylrc ~/.tridactylrc/tridactylrc
+ln -s ${PWD}/tridactylrc ~/.config/tridactyl/tridactylrc
