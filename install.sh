46f96da210f8bdc080b13c9c9815de52147e19ec
Dexter K. Lui <dexterklui@pm.me>
2020-07-18 13:19:50 +0800

    Fix procedure for checking hostname
    Use the content of /proc/sys/kernel/hostname instead of /etc/hostname.

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@
 BASEDIR=$(dirname $0)
 cd $BASEDIR || exit 1
 [[ -z $HOME ]]            && exit 1
-[[ -z $HOST_NAME ]]       && HOST_NAME=$(cat /etc/hostname)
+HOST_NAME=$(cat /proc/sys/kernel/hostname)
 [[ -z $XDG_CONFIG_HOME ]] && XDG_CONFIG_HOME=$HOME/.config
 [[ -z $XDG_DATA_HOME ]]   && XDG_DATA_HOME=$HOME/.local/share
 TMP_DOT_SCRIPT=/tmp/$(whoami)_dot_install.sh
